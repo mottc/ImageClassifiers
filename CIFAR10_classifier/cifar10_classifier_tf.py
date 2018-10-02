b2a66fe62f20c148a812234ddcdd81dd7bcbbf9a
mottc <mottcode@163.com>
2018-10-02 21:31:33 +0800

    修改数据输入网络的方式

--- CIFAR10_classifier/cifar10_classifier_tf.py
+++ CIFAR10_classifier/cifar10_classifier_tf.py
@@ -1,5 +1,4 @@
 import os
-
 import tensorflow as tf
 
 
@@ -28,12 +27,16 @@ dataset = dataset.shuffle(buffer_size=50000)
 dataset = dataset.repeat()
 dataset = dataset.batch(100, drop_remainder=True)
 
-image_holder = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
-label_holder = tf.placeholder(tf.float32, shape=[None, 10])
+iterator = dataset.make_one_shot_iterator()
+image_data, label_data = iterator.get_next()
+
+image_data = tf.reshape(image_data, shape=[-1, 32, 32, 3])
+label_data = tf.reshape(label_data, shape=[-1, 10])
+
 drop_prob = tf.placeholder(tf.float32)
 drop_prob1 = tf.placeholder(tf.float32)
 
-model = tf.layers.conv2d(image_holder, 32, [3, 3], padding='same', activation='relu')
+model = tf.layers.conv2d(image_data, 32, [3, 3], padding='same', activation='relu')
 model = tf.layers.conv2d(model, 32, [3, 3], activation='relu')
 model = tf.layers.max_pooling2d(model, [2, 2], [1, 1], padding='same')
 model = tf.layers.dropout(model, rate=drop_prob)
@@ -48,27 +51,17 @@ model = tf.layers.dense(model, 512, activation='relu')
 model = tf.layers.dropout(model, rate=drop_prob1)
 output = tf.layers.dense(model, 10)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=label_holder))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=label_data))
 train_op = tf.train.RMSPropOptimizer(0.0001, decay=1e-6).minimize(loss)
 
-correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(label_holder, 1))
+correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(label_data, 1))
 acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-iterator = dataset.make_one_shot_iterator()
-next_element = iterator.get_next()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(5000):
-        data = sess.run(next_element)
         if i % 100 == 0:
-            loss_val, train_acc = sess.run([loss, acc], feed_dict={image_holder: data[0],
-                                                                   label_holder: data[1],
-                                                                   drop_prob: 0.0,
-                                                                   drop_prob1: 0.0})
+            loss_val, train_acc = sess.run([loss, acc], feed_dict={drop_prob: 0.0, drop_prob1: 0.0})
             print('loss:', loss_val, '------- acc:', train_acc)
         else:
-            sess.run(train_op, feed_dict={image_holder: data[0],
-                                          label_holder: data[1],
-                                          drop_prob: 0.25,
-                                          drop_prob1: 0.5})
+            sess.run(train_op, feed_dict={drop_prob: 0.25, drop_prob1: 0.5})
